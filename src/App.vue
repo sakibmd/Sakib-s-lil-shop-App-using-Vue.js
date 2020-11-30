1be49e8ae557f67f4a3b7e2cb64a06300cb022e7
sakibmd <sakibmd.cse@gmail.com>
2020-11-30 23:15:41 +0600

    json file (all items) read and prints from in App file

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container my-5">
+    <ul class="list-group">
+      <li class="list-group-item" v-for="(item,index) in items" :key="index">
+        {{ item.title }} - {{ item.price }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import data from "./data";
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    this.items = data;
+  },
+};
+</script>
+
+
+<style>
+
+</style>
